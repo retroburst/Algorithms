90000cd9b7bf0a89a963aa6cb2421d4fdb146b61
Andrew D <retroburst@gmail.com>
2016-01-11 13:15:53 +0900

    Re-wrote bubble and selection sort in C from memory.

---
+++ C/BubbleSort/BubbleSort/main.c
@@ -0,0 +1,60 @@
+//
+//  main.c
+//  BubbleSort
+//
+//  Created by Andrew D on 11/01/2016.
+//  Copyright (c) 2016 retroburst. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdbool.h>
+
+// function prototypes
+void PrintArray(int target[], int length);
+void BubbleSort(int target[], int length);
+
+// main - entry point
+int main(int argc, const char * argv[]) {
+    int numbers[10] = { 12, 445, 55, 67, 2, 7, 909, 45, 4454, 1 };
+    printf("Before bubble sort: ");
+    PrintArray(numbers, 10);
+    BubbleSort(numbers, 10);
+    printf("After bubble sort: ");
+    PrintArray(numbers, 10);
+    return 0;
+}
+
+// prints an array to console
+void PrintArray(int target[], int length)
+{
+    printf("[");
+    for(int i=0; i < length; i++)
+    {
+        printf("%d", target[i]);
+        if(i < length-1) printf(", ");
+    }
+    printf("]\n");
+}
+
+// bubble sorts an array
+void BubbleSort(int target[], int length)
+{
+    bool swapping = false;
+    // while still swapping
+    do{
+        swapping = false;
+        // for each pair of elements (neighbours)
+        for(int i=0; i < length-1; i++)
+        {
+            // if first and bigger than second - swap them
+            if(target[i] > target[i+1])
+            {
+                int temp = target[i];
+                target[i] = target[i+1];
+                target[i+1] = temp;
+                // mark as still swapping
+                swapping = true;
+            }
+        }
+    } while(swapping);
+}
---
+++ C/SelectionSort/SelectionSort/main.c
@@ -0,0 +1,60 @@
+//
+//  main.c
+//  SelectionSort
+//
+//  Created by Andrew D on 11/01/2016.
+//  Copyright (c) 2016 retroburst. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdbool.h>
+
+// function prototypes
+void PrintArray(int target[], int length);
+void SelectionSort(int target[], int length);
+
+// main - entry point
+int main(int argc, const char * argv[]) {
+    int numbers[10] = { 12, 445, 55, 67, 2, 7, 909, 45, 4454, 1 };
+    printf("Before selection sort: ");
+    PrintArray(numbers, 10);
+    SelectionSort(numbers, 10);
+    printf("After selection sort: ");
+    PrintArray(numbers, 10);
+    return 0;
+}
+
+// prints an array to console
+void PrintArray(int target[], int length)
+{
+    printf("[");
+    for(int i=0; i < length; i++)
+    {
+        printf("%d", target[i]);
+        if(i < length-1) printf(", ");
+    }
+    printf("]\n");
+}
+
+// selection sorts an array
+void SelectionSort(int target[], int length)
+{
+    // for each element of array
+    for(int i=0; i < length; i++)
+    {
+        // make sure it is the min of rest of array
+        int indexOfMin = i;
+        for(int j=i; j < length; j++)
+        {
+            // if not min of rest of array, record new min
+            if(target[j] < target[indexOfMin]) { indexOfMin = j; }
+        }
+        // if the min of rest of array was different - swap elements
+        if(indexOfMin != i)
+        {
+            int temp = target[i];
+            target[i] = target[indexOfMin];
+            target[indexOfMin] = temp;
+        }
+    }
+}
